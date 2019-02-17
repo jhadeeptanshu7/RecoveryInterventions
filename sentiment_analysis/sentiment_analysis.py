aada9f34e03d13bf46362403c16b853290cc17dc
deeptanshu <djha@mail.sfsu.edu>
2019-02-17 13:56:51 -0800

    removed the bug added in last commit. lol

--- sentiment_analysis/sentiment_analysis.py
+++ sentiment_analysis/sentiment_analysis.py
@@ -1,3 +1,4 @@
+import sys
 sys.path.append('./../')
 sys.path.append(os.path.dirname(__file__))
 
@@ -28,7 +29,6 @@ import en_core_web_sm
 
 nlp = en_core_web_sm.load()
 from collections import defaultdict
-import sys
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut
 
